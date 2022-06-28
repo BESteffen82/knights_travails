2ea9e904b8253074896cea37216f4b063493b533
Brent Steffen <BESteffen82@gmail.com>
2022-06-28 16:37:02 -0400

    Add Board class

--- main.rb
+++ main.rb
@@ -1,4 +1,29 @@
 require 'pry-byebug'
 
 class Board
+	attr_accessor :rank, :file, :positions
+	
+	def initialize
+		@rank = (1..8).to_a
+	  @file = ("a".."h").to_a		
+		@positions = []
+		assign_positions
+	end
+
+	def assign_positions
+		@positions = rank.product(file)
+		@positions.each do |position|
+			position.reverse!
+		end		
+	end		
 end
+
+class Knight
+	KNIGHT_MOVES = [[2,1],[2,-1],[1,2],[1,-2],[-2,1],[-2,-1],[-1,2],[-1,-2]]
+end
+
+board = Board.new
+p board.assign_positions
+
+
+
