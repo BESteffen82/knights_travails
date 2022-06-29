31b12c5e6e19f6e4966b1cae270af9d209c20ea6
Brent Steffen <BESteffen82@gmail.com>
2022-06-29 11:09:50 -0400

    Add chess_grid variable

--- main.rb
+++ main.rb
@@ -1,29 +1,29 @@
+# frozen_string_literal: true
+
 require 'pry-byebug'
 
 class Board
-	attr_accessor :rank, :file, :positions
-	
-	def initialize
-		@rank = (1..8).to_a
-	  @file = ("a".."h").to_a		
-		@positions = []
-		assign_positions
-	end
+  attr_accessor :rank, :file, :positions
 
-	def assign_positions
-		@positions = rank.product(file)
-		@positions.each do |position|
-			position.reverse!
-		end		
-	end		
+  def initialize
+    @rank = (1..8).to_a
+    @file = ('a'..'h').to_a
+    @positions = []
+    assign_positions
+  end
+
+  def assign_positions
+    @positions = rank.product(file).each(&:reverse!).each_slice(8).to_a.reverse!
+		@chess_grid = @positions.each{|x| x}    
+  end
 end
 
 class Knight
-	KNIGHT_MOVES = [[2,1],[2,-1],[1,2],[1,-2],[-2,1],[-2,-1],[-1,2],[-1,-2]]
+  KNIGHT_MOVES = [[2, 1], [2, -1], [1, 2], [1, -2], [-2, 1], [-2, -1], [-1, 2], [-1, -2]].freeze
+
+	def knight_moves(start_position, end_position)
+		
 end
 
 board = Board.new
-p board.assign_positions
-
-
-
+board.assign_positions
