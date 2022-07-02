53bbb36c4b3cf56779c8ef54dbf962aaea73ca69
Brent Steffen <BESteffen82@gmail.com>
2022-07-02 11:53:15 -0400

    Edit possible_moves method

--- main.rb
+++ main.rb
@@ -3,27 +3,60 @@
 require 'pry-byebug'
 
 class Board
-  attr_accessor :rank, :file, :positions
+  attr_accessor :rank, :file, :coordinates
 
   def initialize
     @rank = (1..8).to_a
     @file = ('a'..'h').to_a
-    @positions = []
+    @coordinates = []
     assign_positions
   end
 
   def assign_positions
-    @positions = rank.product(file).each(&:reverse!).each_slice(8).to_a.reverse!
-		@chess_grid = @positions.each{|x| x}    
+    @coordinates = rank.product(file).each(&:reverse!).each_slice(8).to_a				
   end
 end
 
 class Knight
-  KNIGHT_MOVES = [[2, 1], [2, -1], [1, 2], [1, -2], [-2, 1], [-2, -1], [-1, 2], [-1, -2]].freeze
+	attr_accessor :start_coor, :end_coor, :possible_moves, :moves_made	
+  
+	KNIGHT_MOVES = [[2, 1], [2, -1], [1, 2], [1, -2], [-2, 1], [-2, -1], [-1, 2], [-1, -2]].freeze	
 
-	def knight_moves(start_position, end_position)
+	def initialize
+		board = Board.new
+		@chess_grid = board.coordinates
+		@file_array = board.file		 
+		@possible_moves = []
+		@moves_made = []
+		@move_count = 0			
+	end	
+
+	def knight_coors(start_pos, end_pos)		
+		@start_coor = [@file_array.index(start_pos[0]), start_pos[1] - 1]
+		@end_coor = [@file_array.index(end_pos[0]), end_pos[1] - 1]
+		possible_moves(start_coor)				
+	end
+
+	def possible_moves(start_coor)
+		return nil if @start_coor == @end_coor
 		
+		@possible_moves.push(start_coor)				
+		until @possible_moves.empty?			
+			if @move_count >= 1
+				@start_coor = @possible_moves[0].shift							
+			else @start_coor = @possible_moves.shift
+			end
+			moves = KNIGHT_MOVES.map do |coors|			 
+				coors.map.with_index{|coor, i| coor + @start_coor[i].to_i}
+			end
+			@possible_moves.push(moves)
+			@moves_made.push(@start_coor)
+			@move_count += 1
+			binding.pry		  								
+		end
+		@possible_moves
+	end																				 		  									
 end
 
-board = Board.new
-board.assign_positions
+knight = Knight.new
+knight.knight_coors(["d", 4], ["e", 6])
